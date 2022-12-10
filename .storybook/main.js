01e8360827b401c9b95d46972ef4ef0f1c263684
Amy Dutton <amy@ahhacreative.com>
2022-12-09 21:54:49 -0600

    ➕ Added Vitest, Storybook, Tailwind, Plop

---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.@(js|jsx|ts|tsx|mdx)"
+    "../app/components/**/*.stories.@(js|jsx|ts|tsx|mdx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-vite"
+  },
+  "features": {
+    "storyStoreV7": true
+  }
+}
